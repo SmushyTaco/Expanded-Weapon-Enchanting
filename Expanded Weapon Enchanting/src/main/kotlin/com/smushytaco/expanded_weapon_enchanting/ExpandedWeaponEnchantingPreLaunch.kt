9ed6c04cdbd54162e055ce74798e7717b022acd1
Nikan Radan <github.com@alias.nikanradan.com>
2024-05-30 16:15:09 -0700

    1.20.6

--- Expanded Weapon Enchanting/src/main/kotlin/com/smushytaco/expanded_weapon_enchanting/ExpandedWeaponEnchantingPreLaunch.kt
+++
@@ -1,5 +0,0 @@
-package com.smushytaco.expanded_weapon_enchanting
-import com.llamalad7.mixinextras.MixinExtrasBootstrap
-import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint
-@Suppress("UNUSED")
-object ExpandedWeaponEnchantingPreLaunch: PreLaunchEntrypoint { override fun onPreLaunch() { MixinExtrasBootstrap.init() } }
