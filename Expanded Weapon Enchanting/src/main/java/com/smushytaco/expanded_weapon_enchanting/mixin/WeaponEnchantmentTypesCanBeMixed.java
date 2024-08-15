c8a8628b13d7247365956bf0d7c220569a789ccd
Nikan Radan <personal@nikanradan.com>
2024-08-15 15:49:38 -0700

    1.21.1

--- Expanded Weapon Enchanting/src/main/java/com/smushytaco/expanded_weapon_enchanting/mixin/WeaponEnchantmentTypesCanBeMixed.java
+++ Expanded Weapon Enchanting/src/main/java/com/smushytaco/expanded_weapon_enchanting/mixin/WeaponEnchantmentTypesCanBeMixed.java
@@ -8,6 +8,7 @@ import net.minecraft.item.SwordItem;
 import net.minecraft.registry.entry.RegistryEntry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import java.util.Set;
 @Mixin(Enchantment.class)
 public abstract class WeaponEnchantmentTypesCanBeMixed {
     @ModifyReturnValue(method = "isAcceptableItem", at = @At("RETURN"))
@@ -22,23 +23,6 @@ public abstract class WeaponEnchantmentTypesCanBeMixed {
     @ModifyReturnValue(method = "canBeCombined", at = @At("RETURN"))
     private static boolean hookCanBeCombined(boolean original, RegistryEntry<Enchantment> first, RegistryEntry<Enchantment> second) {
         if (!ExpandedWeaponEnchanting.INSTANCE.getConfig().getCanMixDifferentWeaponEnchantmentTypes()) return original;
-        if (first.matchesKey(Enchantments.SHARPNESS)) {
-            if (second.matchesKey(Enchantments.SMITE) || second.matchesKey(Enchantments.BANE_OF_ARTHROPODS) || second.matchesKey(Enchantments.IMPALING)) return true;
-        } else if (first.matchesKey(Enchantments.SMITE)) {
-            if (second.matchesKey(Enchantments.SHARPNESS) || second.matchesKey(Enchantments.BANE_OF_ARTHROPODS) || second.matchesKey(Enchantments.IMPALING)) return true;
-        } else if (first.matchesKey(Enchantments.BANE_OF_ARTHROPODS)) {
-            if (second.matchesKey(Enchantments.SHARPNESS) || second.matchesKey(Enchantments.SMITE) || second.matchesKey(Enchantments.IMPALING)) return true;
-        } else if (first.matchesKey(Enchantments.IMPALING)) {
-            if (second.matchesKey(Enchantments.SHARPNESS) || second.matchesKey(Enchantments.SMITE) || second.matchesKey(Enchantments.BANE_OF_ARTHROPODS)) return true;
-        } else if (second.matchesKey(Enchantments.SHARPNESS)) {
-            if (first.matchesKey(Enchantments.SMITE) || first.matchesKey(Enchantments.BANE_OF_ARTHROPODS) || first.matchesKey(Enchantments.IMPALING)) return true;
-        } else if (second.matchesKey(Enchantments.SMITE)) {
-            if (first.matchesKey(Enchantments.SHARPNESS) || first.matchesKey(Enchantments.BANE_OF_ARTHROPODS) || first.matchesKey(Enchantments.IMPALING)) return true;
-        } else if (second.matchesKey(Enchantments.BANE_OF_ARTHROPODS)) {
-            if (first.matchesKey(Enchantments.SHARPNESS) || first.matchesKey(Enchantments.SMITE) || first.matchesKey(Enchantments.IMPALING)) return true;
-        } else if (second.matchesKey(Enchantments.IMPALING)) {
-            if (first.matchesKey(Enchantments.SHARPNESS) || first.matchesKey(Enchantments.SMITE) || first.matchesKey(Enchantments.BANE_OF_ARTHROPODS)) return true;
-        }
-        return original;
+        return original || ExpandedWeaponEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.SHARPNESS, Enchantments.SMITE, Enchantments.BANE_OF_ARTHROPODS, Enchantments.IMPALING));
     }
 }
