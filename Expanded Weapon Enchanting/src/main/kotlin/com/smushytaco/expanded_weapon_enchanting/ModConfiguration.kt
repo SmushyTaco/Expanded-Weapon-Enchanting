e77417962d3143e3a652833116caf9b24e18d590
Nikan Radan <github.com@alias.nikanradan.com>
2024-11-25 16:16:45 -0800

    New Config

---
+++ Expanded Weapon Enchanting/src/main/kotlin/com/smushytaco/expanded_weapon_enchanting/ModConfiguration.kt
@@ -0,0 +1,17 @@
+package com.smushytaco.expanded_weapon_enchanting
+import io.wispforest.owo.config.annotation.*
+@Modmenu(modId = ExpandedWeaponEnchanting.MOD_ID)
+@Config(name = ExpandedWeaponEnchanting.MOD_ID, wrapperName = "ModConfig")
+@Suppress("UNUSED")
+class ModConfiguration {
+    @JvmField
+    var canUseImpalingOnWeapon = true
+    @JvmField
+    var canUseDensityOnWeapon = true
+    @JvmField
+    var canUseBreachOnWeapon = true
+    @JvmField
+    var canUseWindBurstOnWeapon = true
+    @JvmField
+    var canMixDifferentWeaponEnchantmentTypes = true
+}
